ea08c54eff9b844afe99eb67b73ff2a076fef891
oidr <oidrnur@gmail.com>
2023-06-10 20:42:28 +0500

    readme, changelog, comments in main

--- caesar_encryption.py
+++ caesar_encryption.py
@@ -1,12 +1,17 @@
 def engine(text: str, shift: int) -> str:
+    """Функция, непосредственно выполняющая преобразование сообщения"""
+
+    # Инициализация алфавитов
     latin_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     latin_lower = 'abcdefghijklmnopqrstuvwxyz'
     cyrillic_upper = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     cyrillic_lower = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     digits = '0123456789'
 
+    # Посимвольный перебор строки с записью результата
     result = ''
     for symbol in text:
+        # Проверка принадлежности символа к алфавиту
         alphabet = None
         if symbol in latin_upper:
             alphabet = latin_upper
@@ -21,13 +26,13 @@ def engine(text: str, shift: int) -> str:
 
         if alphabet:
             index = alphabet.find(symbol)
+            # вычисления локального для данного алфавита сдвига
             local_shift = shift
-
             while local_shift >= len(alphabet):
                 local_shift -= len(alphabet)
             while local_shift < 0:
                 local_shift += len(alphabet)
-
+            # преобразование символа по сдвигу с учётом границ алфавита и запись в result
             if local_shift + index >= len(alphabet):
                 result += alphabet[local_shift+index-len(alphabet)]
             elif local_shift + index < 0:
@@ -41,8 +46,10 @@ def engine(text: str, shift: int) -> str:
 
 
 def encryption_caesar(text: str, shift: int) -> str:
+    # вызывается основная функция и возвращается её результат
     return engine(text, shift)
 
 
 def decryption_caesar(text: str, shift: int) -> str:
+    # вызывается основная функция, но со сдвигом в противоположном знаке, возвращается её результат
     return engine(text, shift*-1)
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from main import *
+from caesar_encryption import *
 import pytest
 
 
